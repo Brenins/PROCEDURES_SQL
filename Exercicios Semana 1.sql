a5f848d33f23ad34adeb29c59ebf0157cafe2d05
Brenins <70190197+Brenins@users.noreply.github.com>
2021-07-22 21:38:32 -0300

    Update
    Novo Exercício

--- Exercicios Semana 1.sql
+++ Exercicios Semana 1.sql
@@ -52,3 +52,20 @@
 
 
 
+CREATE PROCEDURE SP_UPDATE_FILMES_VALORES_10 (@categoria VARCHAR(100))
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    UPDATE filme
+       SET VALOR = Valor + ((valor/100) * 10)
+    WHERE categoriaId = (
+    SELECT
+        categoria.id
+    FROM categoria
+    WHERE categoria.descricao like '%'+@categoria+'%'
+)
+END
+
+EXEC SP_UPDATE_FILMES_VALORES_10 Ação
+
